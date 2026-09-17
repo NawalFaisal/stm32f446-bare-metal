4aa0086b57996053cfdfac338da42a1be4aad5e5
NawalFaisal <nawal.fais@outlook.com>
2026-09-17 12:18:17 -0600

    feat(uart): add polled USART driver with init, read, and write

--- drivers/uart.c
+++ drivers/uart.c
@@ -1,8 +1,25 @@
 #include "uart.h"
+#include "stdint.h"
+#define SYSTEM_CLOCK_HZ 16000000
 
 
-//the abstarctions i want to create are 
+void usart_init(TypeDef_USART *usart, baud_rate_t baud){
+    uint8_t divisor = SYSTEM_CLOCK_HZ / baud;
+    usart -> BRR = divisor;  
 
-//uart inilize 
-//uart read 
-//uart write
+    usart->CR1 |= (1U << 2);    /* RE */
+    usart->CR1 |= (1U << 3);    /* TE */
+    usart->CR1 |= (1U << 13);   /* UE */
+    
+}
+
+void usart_write_char(TypeDef_USART *usart, char c){
+    while(!(usart -> SR & (1U << 7))){ }  /*wait for TXE*/
+        usart -> DR = c; 
+}
+
+void usart_read_char(TypeDef_USART *usart){
+    while(!(usart -> SR & (1U << 5))) {}
+    return usart -> DR; 
+
+}
--- include/uart.h
+++ include/uart.h
@@ -10,6 +10,20 @@ typedef struct {
     volatile uint32_t CR1;   // offset 0x0C   //configures if its transmitting or reciveing
 } TypeDef_USART;
 
+#define USART1 ((TypeDef_USART *)0x40011000 )
 #define USART2 ((TypeDef_USART *)0x40004400)
+#define USART3 ((TypeDef_USART *)0x40004800)
+
+//enum for baud rates
+typedef enum {
+    BAUD_9600   = 9600,
+    BAUD_19200  = 19200,
+    BAUD_38400  = 38400,
+    BAUD_57600  = 57600,
+    BAUD_115200 = 115200
+} baud_rate_t;
+
+
+
 
 #endif //UART_H
